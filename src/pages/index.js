9eb6164bccb894b3be4a1b6e01fd14e99fc94551
MuhammadAli-ai <muhammad.a.ali88@gmail.com>
2022-05-25 10:59:16 +0100

    added additional search functionality of sub-region

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,35 @@ import Layout from "../components/Layout/Layout";
 import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
+import { useState } from "react";
 
 export default function Home({ countries }) {
   console.log(countries);
+
+  const [keyword, setKeyword] = useState("");
+
+  // filter country by name or region
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (event) => {
+    event.preventDefault();
+
+    setKeyword(event.target.value.toLowerCase());
+  };
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput placeholder="Filter by Name, Region or Subregion" />
-      <CountriesTable countries={countries} />
+      <SearchInput
+        placeholder="Filter by Name, Region or Subregion"
+        onChange={onInputChange}
+      />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
